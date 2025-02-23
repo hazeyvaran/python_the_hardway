58d3efdbfc221f33169229aaddc21bb569b9b9e1
hazeyvaran <hazey_varan44@proton.me>
2025-02-22 23:50:34 -0500

    examples added for required and optional items

--- argparse_only.py
+++ argparse_only.py
@@ -2,3 +2,19 @@ import argparse
 
 # create an argumentParser object
 parser = argparse.ArgumentParser(description="Example script with argument")
+
+# Add a positional argument (required)
+parser.add_argument("name", type=str, help="Your name")
+
+# Adds an optional argument with a default value
+parser.add_argument("--age", type=int, help="enter your age", default=None)
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Output the results
+print(f"Hello, {args.name}!")
+if args.age:
+    print(f"you are {args.age} years old")
+else:
+    print("age was not provided")
