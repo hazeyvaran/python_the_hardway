43d7a6160d982c38e96af31d1d9d45f94cbce517
hazeyvaran <hazey_varan44@proton.me>
2025-02-22 19:12:36 -0500

    thrid commit

--- argparser.py
+++ argparser.py
@@ -1,4 +1,20 @@
 import sys
+#
+# print("script name:", sys.argv[0])
+# print("argument:", sys.argv[1:])
 
-print("script name:", sys.argv[0])
-print("argument:", sys.argv[1:])
+
+####################################
+
+#accessing individual arguments
+if len(sys.argv) < 3:
+    print("usage: python script <arg1> <arg2>")
+    sys.exit(1)
+
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+print(f"first argument: {arg1}")
+print(f"second argument: {arg2}")
+
+
